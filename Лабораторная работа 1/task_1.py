98065a0e1e88e7d9c726e5fc9459883a69e56fd6
karim <viouxxie@gmail.com>
2024-10-20 22:55:03 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_1.py
@@ -0,0 +1,9 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+index = numbers.index(None)
+numbers[index] = 0
+numbers[index]=sum(numbers)/len(numbers)
+
+
+print("Измененный список:", numbers)
